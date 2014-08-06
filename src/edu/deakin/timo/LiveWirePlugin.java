57374e911993084acbde7304f254ee8d1cc76307
Timo Rantalainen <tjrantal@gmail.com>
2014-08-06 15:51:51 +1000

    Implemented backpedaling clicks

--- src/edu/deakin/timo/LiveWirePlugin.java
+++ src/edu/deakin/timo/LiveWirePlugin.java
@@ -21,7 +21,7 @@ import edu.deakin.timo.liveWireEngine.*;	/**Live wire implementation*/
 /*
 	LiveWire ImageJ plug-in modified from ivus snakes (http://ivussnakes.sourceforge.net/) ImageJ plugin 
 	Changed the implementation back to the one suggested in Barret & Mortensen 1997.
-	Interactive live-wire boundary extraction. Medical Image Analysis (1996/7) volume 1, number 4, pp 331–341.
+	Interactive live-wire boundary extraction. Medical Image Analysis (1996/7) volume 1, number 4, pp 331-341.
  */
 
 public class LiveWirePlugin implements PlugIn, MouseListener, MouseMotionListener {
@@ -29,6 +29,7 @@ public class LiveWirePlugin implements PlugIn, MouseListener, MouseMotionListene
 	ImagePlus imp;
 	PolygonRoi roi;
 	Polygon polygon;
+	ArrayList<Polygon> polygons;
 	int width;
 	int height;
 	LiveWireCosts lwc;
@@ -59,6 +60,9 @@ public class LiveWirePlugin implements PlugIn, MouseListener, MouseMotionListene
 			}
 		}
 		
+		/*Init polygon stack for history*/
+		polygons = new ArrayList<Polygon>();
+		
 		/*Init livewire*/
 		lwc = new LiveWireCosts(pixels);
     }
@@ -66,7 +70,7 @@ public class LiveWirePlugin implements PlugIn, MouseListener, MouseMotionListene
 	/*Implement the MouseListener, and MouseMotionListener interfaces*/
 	public void mousePressed(MouseEvent e) {
 		/*If alt is pressed when a button is clicked, disconnect listeners, i.e. stop the plugin*/
-		if (e.getClickCount() > 1 || e.getButton() == MouseEvent.BUTTON2 ||  (e.getModifiersEx() & InputEvent.ALT_MASK) != 0||  (e.getModifiersEx() & InputEvent.ALT_DOWN_MASK) != 0){
+		if (e.getClickCount() > 1){
 			if (false){
 				//Remove the last point added with the first click of the double click
 				int[] pX = new int[polygon.npoints];
@@ -101,39 +105,65 @@ public class LiveWirePlugin implements PlugIn, MouseListener, MouseMotionListene
 
 	public void mouseReleased(MouseEvent e) {
 		int screenX = e.getX();
-		int screeY = e.getY();
+		int screenY = e.getY();
 		int x = canvas.offScreenX(screenX);
-		int y = canvas.offScreenY(screeY);
-		
-		
-		//IJ.log("Mouse released: "+x+","+y+" polygon length "+polygon.npoints);
-		/*Draw the latest Polygon*/
-		if (polygon.npoints > 0){
-			if ((e.getModifiersEx() & InputEvent.SHIFT_MASK) != 0||  (e.getModifiersEx() & InputEvent.SHIFT_DOWN_MASK) != 0){
-				//Get a straight line
-				polygon.addPoint(x,y);
-			}else{
-				//Get polygon from livewire
-				int[][] fromSeedToCursor = lwc.returnPath(x,y);
-				int[] pX = new int[polygon.npoints+fromSeedToCursor.length];
-				int[] pY = new int[polygon.npoints+fromSeedToCursor.length];
-				for (int i = 0;i< polygon.npoints;++i){
-					pX[i] = polygon.xpoints[i];
-					pY[i] = polygon.ypoints[i];
+		int y = canvas.offScreenY(screenY);
+		/*Backpedal polygon to the previous one*/
+		if(polygons.size()>0 && ((e.getModifiersEx() & InputEvent.CTRL_MASK) != 0||  (e.getModifiersEx() & InputEvent.CTRL_DOWN_MASK) != 0)){
+				//Get the previous polygon
+				Polygon tempP = polygons.get(polygons.size()-1);
+				int[] pX = new int[tempP.npoints];
+				int[] pY = new int[tempP.npoints];
+				for (int i = 0;i< tempP.npoints;++i){
+					pX[i] = tempP.xpoints[i];
+					pY[i] = tempP.ypoints[i];
 				}
-				for (int i = 0;i< fromSeedToCursor.length;++i){
-					pX[polygon.npoints+i] = fromSeedToCursor[i][0];
-					pY[polygon.npoints+i] = fromSeedToCursor[i][1];
-				}
-				polygon = new Polygon(pX, pY, pX.length);
-			}
-			/**Get, and set the ROI*/
-			roi = new PolygonRoi(polygon,Roi.POLYLINE);
-			imp.setRoi(roi,true);
+				polygon = new Polygon(pX,pY,pX.length);
+				polygons.remove(polygons.size()-1);	//Remove the previous polygon
+				roi = new PolygonRoi(polygon,Roi.POLYLINE);
+				imp.setRoi(roi,true);
+				lwc.setSeed(pX[pX.length-1],pY[pX.length-1]);
 		}else{
-			polygon.addPoint(x,y);
+			//Draw the latest Polygon
+			if (polygon.npoints > 0){
+				//Store a copy of the previous polygon
+				int[] tX = new int[polygon.npoints];
+				int[] tY = new int[polygon.npoints];
+				for (int i = 0;i< polygon.npoints;++i){
+					tX[i] = polygon.xpoints[i];
+					tY[i] = polygon.ypoints[i];
+				}
+				polygons.add(new Polygon(tX,tY,tX.length));	//Store the previous polygon
+				//Update the polygon
+				if ((e.getModifiersEx() & InputEvent.SHIFT_MASK) != 0||  (e.getModifiersEx() & InputEvent.SHIFT_DOWN_MASK) != 0){
+					//Get a straight line
+					polygon.addPoint(x,y);
+				}else{
+					//Get polygon from livewire
+					int[][] fromSeedToCursor = lwc.returnPath(x,y);
+					int[] pX = new int[polygon.npoints+fromSeedToCursor.length];
+					int[] pY = new int[polygon.npoints+fromSeedToCursor.length];
+					for (int i = 0;i< polygon.npoints;++i){
+						pX[i] = polygon.xpoints[i];
+						pY[i] = polygon.ypoints[i];
+					}
+					for (int i = 0;i< fromSeedToCursor.length;++i){
+						pX[polygon.npoints+i] = fromSeedToCursor[i][0];
+						pY[polygon.npoints+i] = fromSeedToCursor[i][1];
+					}
+					polygon = new Polygon(pX, pY, pX.length);
+				}
+				//Get, and set the ROI
+				roi = new PolygonRoi(polygon,Roi.POLYLINE);
+				imp.setRoi(roi,true);
+				
+			}else{
+				polygon.addPoint(x,y);
+				lwc.setSeed(x,y);
+			}
+			lwc.setSeed(x,y);
+			
 		}
-		lwc.setSeed(x,y);
 	}
 	
 	public void mouseDragged(MouseEvent e) {
@@ -146,9 +176,9 @@ public class LiveWirePlugin implements PlugIn, MouseListener, MouseMotionListene
 	public void mouseMoved(MouseEvent e) {
 		if (polygon.npoints > 0){
 			int screenX = e.getX();
-			int screeY = e.getY();
+			int screenY = e.getY();
 			int x = canvas.offScreenX(screenX);
-			int y = canvas.offScreenY(screeY);
+			int y = canvas.offScreenY(screenY);
 			int[] pX;
 			int[] pY;
 			if ((e.getModifiersEx() & InputEvent.SHIFT_MASK) != 0||  (e.getModifiersEx() & InputEvent.SHIFT_DOWN_MASK) != 0){
